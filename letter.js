7d36bff78cb354ca8ec740ae45795e2d1254462b
Irusha Vidanamadura <irusha.dilshan@gmail.com>
2017-01-15 19:12:00 -0500

    Added Cover Letter Contents

---
+++ letter.js
@@ -0,0 +1,36 @@
+$(function(){
+  $(document).ready(function() {
+    $(".type-area").typed({
+      stringsElement: $('#type-content'),
+      typeSpeed: 0,
+      contentType: 'html',
+      loop: false
+    });
+    var lastScrollEvent;
+    function startAutoScroll() {
+      autoScroll = setInterval(function() {
+        window.scroll(0, document.body.scrollHeight);
+      }, 1);
+    }
+    function clearAutoScroll() {
+      if (autoScroll) {
+        clearInterval(autoScroll);
+        window.scroll(0, document.body.scrollHeight);
+      }
+    }
+    $('.sticky-btn').click(function() {
+      if (lastScrollEvent === 'start') {
+        lastScrollEvent = 'stop';
+        clearAutoScroll();
+        $('.sticky-btn').text('Start Auto Scrolling');
+      }
+      else {
+        lastScrollEvent = 'start';
+        startAutoScroll();
+        $('.sticky-btn').text('Stop Auto Scrolling');
+      }
+    });
+    lastScrollEvent = 'start';
+    startAutoScroll();
+  });
+});
